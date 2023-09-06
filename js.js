96b28c309bab6a0ee6ffb61380be0ac2d3f0a701
Jonathon <jcraig94@hotmail.com>
2023-09-05 18:10:37 -0700

    first commit

---
+++ js.js
@@ -0,0 +1,74 @@
+let buttons = document.querySelectorAll("button");
+let submit = document.querySelector(".alter");
+let change = document.querySelector(".number");
+let red = document.querySelector('.red');
+let orange = document.querySelector('.orange');
+let black = document.querySelector('.black');
+let dark = document.querySelector('.dark');
+let normalColor = document.querySelector('.normalcolors')
+let darken = false;
+
+function placeSquares(size) {
+    let board = document.querySelector(".board");
+    let squares = document.querySelectorAll('.board div');
+    squares.forEach((div)  => div.remove());
+    board.style.gridTemplateColumns  = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows  = `repeat(${size} , 1fr)`;
+    let amount = size * size;
+    for (let i = 0; i < amount; i++){
+        let square = document.createElement("div");
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", square);
+        
+        normalColor.addEventListener("click",  () => {
+            darken = false;
+        })
+        
+        let effect =  dark.addEventListener("click",  () => {
+            darken = true
+            if (darken === true) {
+            square.addEventListener ("mouseover", () => {
+                    let currentBrightness = orange.style.brightness || 100;
+                    currentBrightness = parseInt(currentBrightness) - 10;
+                    if (currentBrightness >= 0 ) {
+                        square.style.filter = `brightness(${currentBrightness}%)`;
+                        orange.style.brightness = currentBrightness;   
+                        console.log(effect)
+            }
+        })
+    }
+    })
+        orange.addEventListener("click",  () => { 
+            square.addEventListener ("mouseover", () => {
+                square.style.backgroundColor = "orange";
+                
+        })
+    });
+
+        red.addEventListener("click", () => {
+            square.addEventListener ("mouseover", () => {
+                square.style.backgroundColor = "red"; 
+         })
+
+    });
+
+        black.addEventListener("click", () => {
+        square.addEventListener ("mouseover", () => {
+        square.style.backgroundColor = "black";
+        })
+    });
+
+    
+}   
+
+}
+
+submit.addEventListener ("click", () => {
+    if  (change.value > 100) return "Error";
+    placeSquares (change.value);
+});
+
+
+
+
+placeSquares(16);
